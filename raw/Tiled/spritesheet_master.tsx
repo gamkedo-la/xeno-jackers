b2fdc4a804705104b05ef9f6e4659d3f66329007
jghanlon <axphin@yahoo.com>
2020-05-13 19:45:21 -0700

    updated master spritesheet and BAR level

---
+++ raw/Tiled/spritesheet_master.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="spritesheet_master" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="../../img/backgrounds/spritesheet_master.png" width="512" height="512"/>
+</tileset>
