516538e0c5cdf910f0dde9e0fd316ca3a96e8d13
EyeForcz <61577998+EyeForcz@users.noreply.github.com>
2020-06-15 16:55:50 +0200

    improvely coded the codeblock code of the cheatCode code for the code section.

--- js/cheats.js
+++ js/cheats.js
@@ -1,25 +1,42 @@
+// The cheatcode buffer to store strings between key presses
 var cheatBuffer = "";
 
-// Note: single character cheats dont work, must be atleast 2 characters and all lowercase
-var cheatList = [
-	"kill", 		// 0 - opens the gameover screen
-	"speed",		// 1 - gives the palyer speedburst for 600s
-	"UUDDLRLRba",	// 2 - Konami code
-	"shield",		// 3 - get super shield
-	"chris",		// 4 - super secret chris weapon
-	"atom",			// 5 - atomic weapon
-	"mid",			// 6 - mid weapon
-	"laser",		// 7 - laser weapon
-];
-var mouseAction ={
-	selected: undefined,
-	action: undefined
-};
+//set this to every debug only cheatcode in the "active:" section
+//for quick on and off switching
+var DEBUG_CHEATS = true;
 
+//INFO: This is the cheat list array in which you can simply copy
+//the whole "cheat ={...}" part and insert your own stuff.
+//code: The cheatcode "code", must be all lowercase because uppercase is reserved to special buttons.
+//active: Tells if the cheatcode is enabled or disabled. Action will not run if disabled. Takes true or false.
+//action: Insert the code that should run when a cheatcode gets activated here.
+
+var cheatList = [
+	cheat ={
+		//This could kill someone
+		code: 'kill',
+		active: true,
+		action: function(){
+			console.log("Look mom");
+			console.log("im cheating");
+		},
+	},
+	cheat ={
+		// "i" like i need coffee
+		code: 'i',
+		active: DEBUG_CHEATS,
+		action: function(){
+			console.log("you dont look");
+			console.log("like a dev");
+		},
+	}, 			
+];
+
+//The cheatcode function, nothing interresting here.
 function cheats(key) {
 	var mightMatchCode = 0; //counts possible solutions
 	var keyBuffer = "";
-
+	
 	// Turn inputs to lowercase and special case buttons to uppercase letters
 	if(key.length == 1){
 		keyBuffer = key.toLowerCase();
@@ -35,65 +52,41 @@ function cheats(key) {
 
 	
 	if(keyBuffer != " "){
+		//add the pressed key to the current buffer string
 		cheatBuffer += keyBuffer;
 		//run a check for each cheatcode in the array
 		cheatList.forEach (function (val,index){
-			//Give points for each index
+
+			//Give points for each cheatCode in the array
 			mightMatchCode++;
 
 			//check if cheatbuffer string matches to an element
 			for (var i = 0; i < cheatBuffer.length++; i++){	
-				if (cheatBuffer.charAt(i) == val.charAt(i)){
+				if (cheatBuffer.charAt(i) == val.code.charAt(i)){
 					
 					//If cheatcode fully matches
-					if(cheatBuffer == val){		
-						console.log("cheat activated: " + val);
-						switch (index) {
-							case 0: //cheat: kill 
-								console.log("testi");
-								break;	
-							case 1: //cheat: speed
-								p1.addSpeed(600);
-								break;
-							case 2: //cheat: konami
-								p1.addSpeed(600);
-								break;
-							case 3: //cheat: Super shield
-								p1.addShield(6);
-								break;
-							case 4: //cheat: Chris
-								p1.chrisCode = true;
-								p1.addWeapon("chris", 1)
-								p1.addShield(6);
-								break;
-							case 5: //cheat: atom weapon
-								p1.addWeapon("atom", 1)
-								break;
-							case 6: //cheat: mid weapon
-								p1.addWeapon("mid", 1)
-								break;
-							case 7: //cheat: laser weapon
-								p1.addWeapon("laser", 1)
-								break;
+					if(cheatBuffer == val.code){
+						//is cheatcode enabled?		
+						if(val.active == true){
+							console.log("cheat activated: " + val.code);
+							val.action();			
 						}
 						cheatBuffer = "";
-						break;				
+							break;				
 					}
-
 				}else{
-					//remove point if string doesnt match
+					//remove point if string doesnt matched
 					mightMatchCode--;
 					break;						
 				}
 			}
 		});
-			
-		//console.log(`buffer: ${cheatBuffer}` + " " + mightMatchCode+ " ");
 
-		// reset complete string if nothing matched.
-		if (mightMatchCode == 0 && keyBuffer.length == 1 && keyBuffer != " ") {
-			cheatBuffer = keyBuffer;
-			//console.log("bufferReset")
+		// reset complete string if nothing matched
+		if (mightMatchCode == 0 && keyBuffer.length == 1 && keyBuffer != " " && cheatBuffer.length > 1) {
+			cheatBuffer = "";
+			//We run this to see if it was a single letter cheatCode
+			cheats(keyBuffer);
 		}
 	}
 }
