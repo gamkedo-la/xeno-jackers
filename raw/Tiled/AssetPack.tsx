c7df212879077a422e50058f8a4aa663311948a1
jghanlon <axphin@yahoo.com>
2020-07-05 09:59:53 -0700

    Created WallOrbSpawn and added it to the Asset Pack

--- raw/Tiled/AssetPack.tsx
+++ raw/Tiled/AssetPack.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="AssetPack" tilewidth="34" tileheight="34" tilecount="12" columns="0">
+<tileset version="1.4" tiledversion="1.4.1" name="AssetPack" tilewidth="34" tileheight="34" tilecount="13" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="player">
   <image width="21" height="30" source="../img/characters/PlayerSpawnImage.png"/>
@@ -36,5 +36,8 @@
  </tile>
  <tile id="11" type="flyingEnemy">
   <image width="34" height="23" source="../img/characters/FlyingEnemySpawn.png"/>
+ </tile>
+ <tile id="12" type="wallOrb">
+  <image width="16" height="16" source="../img/characters/WallOrbSpawn.png"/>
  </tile>
 </tileset>
