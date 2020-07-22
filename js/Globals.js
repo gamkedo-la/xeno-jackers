8b159f88f0369fff44dff500a2b196b9df81b7c8
jghanlon <axphin@yahoo.com>
2020-07-21 17:38:15 -0700

    prepping global consts for additional music tracks

--- js/Globals.js
+++ js/Globals.js
@@ -13,6 +13,10 @@ const MAX_Y_SPEED = 95; //65 - launch off ground speed for player jump
 const COLLIDER_COLOR = 'aqua';
 const MENU_MUSIC_FILENAME = 'audio/Shadow_51';
 const LEVEL_1_MUSIC_FILENAME = 'audio/level1MusicV1';
+const LEVEL_2_MUSIC_FILENAME = 'audio/level1MusicV1';
+const LEVEL_3_MUSIC_FILENAME = 'audio/Shadow_51';
+const GAMEOVER_MUSIC_FILENAME = 'audio/Shadow_51';
+const ENDING_MUSIC_FILENAME = 'audio/Shadow_51';
 
 const GAME_SCALE = 1;
 
